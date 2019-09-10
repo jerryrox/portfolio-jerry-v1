89d43e82cd66dd7c963cbb57052612c67a388d80
Jerry Kim <jerryrox@live.co.kr>
2019-09-10 19:45:53 +1200

    Updates

---
+++ precache-manifest.774c5b33ca47f6d2c759e392cedd3328.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70feefb1b7cb46d5add833c5bdedc6c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ebf70c902debbe23057",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3dd2446992aa2a386b8a",
+    "url": "/portfolio/static/css/main.ebbf69bd.chunk.css"
+  },
+  {
+    "revision": "6ebf70c902debbe23057",
+    "url": "/portfolio/static/js/2.0e0839f9.chunk.js"
+  },
+  {
+    "revision": "3dd2446992aa2a386b8a",
+    "url": "/portfolio/static/js/main.556d1357.chunk.js"
+  },
+  {
+    "revision": "fc078ad60574ac74917a",
+    "url": "/portfolio/static/js/runtime~main.ebbfba6b.js"
+  },
+  {
+    "revision": "4bd7a09c722e1a58a10e63142dd915d3",
+    "url": "/portfolio/static/media/home.4bd7a09c.jpg"
+  },
+  {
+    "revision": "6fe20ee99b752f5857126969d3699813",
+    "url": "/portfolio/static/media/portrait.6fe20ee9.png"
+  }
+]);
